ed107d0a112e831d055a1430010b8ee3130a01c1
Tom Brinkmann <tombrinkmann@MacBook-Air.local>
2025-07-27 20:17:25 +0200

    Fix asset paths with relative URLs

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/kalender-agent-app/',
+  base: '',
   server: {
     port: 5183,
     open: true,
